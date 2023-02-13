2900dc54935c710845ad8fd5ff45bd188f02fac9
Paulo Antonio <dev.pauloantonio785@gmail.com>
2023-02-12 22:13:11 -0300

    Inclusão das imagens do assets na nuvem do google drive!

--- app.js
+++ app.js
@@ -34,11 +34,12 @@ app.post("/send-email", cors(), async (req, res) => {
 
     
     const TEMPLATE_HTML = `
-    <body style="background-image: url(assets/bg.svg); background-repeat: no-repeat;
-     background-size: cover; display: flex; flex-direction: column; justify-content:center; align-items: center;
-     margin: 0; padding: 25px; font-family: Arial, Helvetica, sans-serif; box-sizing: border-box;">
-    <div style="background-color: white; width: 85%; box-shadow:  1px 1px 6px 7px rgba(79, 79, 79, 0.13); padding: 25px;
-    border-radius: 8px; font-size: 19px; display:flex; flex-direction: row; margin-top: 100px; height: auto; border: 1px solid rgb(109, 109, 109);">
+    <body style="background-image: url(https://lh3.googleusercontent.com/drive-viewer/AAOQEOTv5R8D5xnLURMG8mM4ikIIAqYp3L0LmjBj-u6MnkUbZ94Vegc3RTTwQbVsIoYCGUMD3qoB3F1oU1Cj4cXbj7Gc3ebhHA=w1920-h902); background-repeat: no-repeat;
+    background-size: cover; display: flex; flex-direction: column; justify-content:center; align-items: center;
+    margin: 0; padding: 25px; font-family: Arial, Helvetica, sans-serif; box-sizing: border-box;">
+   <div style="background-color: white; width: 85%; box-shadow:  1px 1px 6px 7px rgba(79, 79, 79, 0.13); padding: 25px;
+   border-radius: 8px; font-size: 19px; display:flex; flex-direction: row; margin-top: 100px; height: auto; border: 1px solid rgb(109, 109, 109);">
+      
        
         <section style="width: 75%;">
             <div class="card-body">
@@ -52,9 +53,9 @@ app.post("/send-email", cors(), async (req, res) => {
 
             </div>
         </section>
-        <section style="width: 68%; background-image: url(/assets/img.png); background-position: center;
+        <section style="width: 68%; background-image: url(https://lh3.googleusercontent.com/drive-viewer/AAOQEOSMHJnpLVTMy3np6y_Ls2-8izyZwRIiyttwVNJqnLQqBG2M-PEGjiOCTd6yru9kc72Fj-O2n5gMwFxKAg6TpKZuvO71zQ=w1920-h902); background-position: center;
          background-repeat: no-repeat; background-size: 100% 100%; display: flex; justify-content: end; align-items: start;">
-            <a href="https://wa.me/${telefone}" style="background-image: url(assets/icon.svg);
+            <a href="https://wa.me/${telefone}" style="background-image: url(https://lh3.googleusercontent.com/drive-viewer/AAOQEORXEq79U5xit0bl10Jql4yY-mgaehLn2wBUX8DfWRrl6aRwWjTz9vFt5-vq1Rb6_7iyPv92nvgoFfNBM79OBotXbtCPnA=w1920-h902);
             background-position: center  bottom 0.1px; background-repeat: no-repeat; border-radius: 8px; width: 5%;
                padding: 25px 20px; border: 1px solid black; margin: 8px;"></a>
         </section>
@@ -65,8 +66,8 @@ app.post("/send-email", cors(), async (req, res) => {
 </body>`;
 
     const mailOptions = {
-        from: process.env.USERNAME,
-        to: email,
+        from: email,
+        to:  process.env.USERNAME,
         subject: subject,
         text: text,
         html: html ? TEMPLATE_HTML : null,
